4455b8cc85e21b74a6c05f2ec29163df5bfb6d57
Sakapatate <49942860+Sakapatate@users.noreply.github.com>
2019-04-24 17:09:53 +0200

    Test Touch Controles Quentin
    Scène simple avec un cube rouge qui se tp la ou on touche sur l'écran.

---
+++ FruityPicker2Unity/Assets/Personnel/Quentin/Scripts/Q_TouchScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Q_TouchScript : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Récupère le touch input si il est hold plus de 0 frames
+        if (Input.touchCount > 0)
+        {
+            
+            Touch touchTouch = Input.GetTouch(0);
+            //change position écran en position world
+
+            Vector3 touchVectorThree = new Vector3 (touchTouch.position.x, touchTouch.position.y, Camera.main.transform.position.z);
+
+            Vector3 vTouchPosition = Camera.main.ScreenToWorldPoint(touchVectorThree);
+
+            //Debug.Log("Camera :"+ Camera.main.ScreenToWorldPoint(touchVectorThree));
+            //Debug.Log("Camera :"+ Camera.main.ScreenToWorldPoint(touchTouch.position));
+
+            Debug.Log("Trad 3D : "+ vTouchPosition);
+            vTouchPosition.z = 0f;
+            transform.position = vTouchPosition;
+
+
+        }
+    }
+}
