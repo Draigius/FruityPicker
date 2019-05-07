262e820bbc8d440969e0f09f18c9f26c51dbd3dd
Sakapatate <49942860+Sakapatate@users.noreply.github.com>
2019-05-07 15:31:57 +0200

    Mise à Jour Partie Perso

---
+++ QuentinUnity/Assets/Scripts/Q_SwipeLogger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Q_SwipeLogger : MonoBehaviour
+{
+    private void Awake()
+    {
+        Q_SwipeScript.OnSwipe += SwipeDetector_OnSwipe;
+    }
+
+    private void SwipeDetector_OnSwipe (SwipeData swipeDataInput)
+    {
+        Debug.Log("Swipe in Direction : " + swipeDataInput.Direction);
+
+    }
+}
---
+++ QuentinUnity/Assets/Scripts/Q_TouchScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Q_TouchScript : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Récupère le touch input si il est hold plus de 0 frames
+        if (Input.touchCount > 0)
+        {
+            
+            Touch tTouch = Input.GetTouch(0);
+            //change position écran en position world
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(tTouch.position), out hit, 1000))
+            {
+                Debug.Log("MoveToClick_Script - Found Point");
+                Vector3 tRayPoint =  hit.point;
+
+                tRayPoint.z = 0;
+
+                transform.position = tRayPoint;
+            }
+
+            //Vector3 touchVectorThree = new Vector3 (touchTouch.position.x, touchTouch.position.y, Camera.main.transform.position.z);
+            //Vector3 vTouchPosition = Camera.main.ScreenToWorldPoint(touchVectorThree);
+
+            //Debug.Log("Trad 3D : "+ vTouchPosition);
+            //vTouchPosition.z = 0f;
+            //transform.position = vTouchPosition;
+
+           
+        
+        }
+    }
+}
