4819ff9b0b777b6db15aa6a818e18c7ce4a1b1dc
Sakapatate <49942860+Sakapatate@users.noreply.github.com>
2019-05-20 10:11:04 +0200

    MAJ Scene Perso Scoring + ProtoTouch

--- QuentinUnity/Assets/Scripts/Inputs/PressoirScript.cs
+++ QuentinUnity/Assets/Scripts/Inputs/PressoirScript.cs
@@ -2,15 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InputManagerFinNiveau : MonoBehaviour
+public class PressoirScript : MonoBehaviour
 {
-    private bool bSecateurIsActive = false;
+    private bool bPressoirIsActive = false;
     private Vector3 v3PositionActuelle;
     private Vector3 v3PositionTarget;
     private Vector3 v3PositionStart;
 
 
-    private float fVitesse = 10f;
+    private float fVitesse = 1f;
     private float fDistanceCorrection;
 
     // Start is called before the first frame update
@@ -18,58 +18,48 @@ public class InputManagerFinNiveau : MonoBehaviour
     {
         // Penser à mettre un dummy pour retrouver position
         //Ou enregistrer posisiton de départ
+
         v3PositionStart = gameObject.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-
         float fStep = fVitesse * Time.deltaTime;
         v3PositionActuelle = gameObject.transform.position;
         //Deplacement
-        if (!bSecateurIsActive && v3PositionActuelle != v3PositionStart)
+        if (!bPressoirIsActive && v3PositionActuelle != v3PositionStart)
         //Move vers position initiale
         {
             transform.position = Vector3.MoveTowards(transform.position, v3PositionStart, fStep);
         }
-        else if (bSecateurIsActive)
+        else if (bPressoirIsActive)
         //Move vers position souris
         {
             ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-            //Debug.Log("Target PreDistance" + v3PositionTarget.x);
-
+            
             v3PositionTarget = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
-
-
-
+            
             Debug.Log("v3PositionTarget.x :" + v3PositionTarget.x);
             Debug.Log("fDistanceCorrection :" + fDistanceCorrection);
             Debug.Log("position :" + v3PositionActuelle);
-
-            //transform.position = Vector3.MoveTowards(transform.position, v3PositionTarget, fStep);
-            transform.position = new Vector3((v3PositionTarget.x * -1) - fDistanceCorrection, v3PositionStart.y, v3PositionStart.z);
+            
+            transform.position = new Vector3( v3PositionStart.x, (v3PositionTarget.y * -1) - fDistanceCorrection, v3PositionStart.z);
 
             ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         }
     }
-
-    //Collision
     
-    public void funcActiveSecateur()
+    public void funcActivePressoir()
     {
-        bSecateurIsActive = true;
+        bPressoirIsActive = true;
 
         v3PositionTarget = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
-        fDistanceCorrection = v3PositionTarget.x * -1 - v3PositionActuelle.x;
-
-        //Debug.Log("Secateur Actif");
+        fDistanceCorrection = v3PositionTarget.y * -1 - v3PositionActuelle.y;
     }
 
-    public void funcDesactiveSecateur()
+    public void funcDesactivePressoir()
     {
-        bSecateurIsActive = false;
-        //Debug.Log("Secateur Inactif");
+        bPressoirIsActive = false;
     }
 }
