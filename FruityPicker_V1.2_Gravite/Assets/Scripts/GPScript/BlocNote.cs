048ac3570561812254cb5fb57806dd8f23d1d8f1
Cécile Dassieu <cecile.dassieu@gmail.com>
2019-05-21 12:56:24 +0200

    Rajout gravité

---
+++ FruityPicker_V1.2_Gravite/Assets/Scripts/GPScript/BlocNote.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlocNote : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    //    int iIdChercher = 0;
+
+
+
+    //    for (int k = 0; k < 50; k++)
+    //    {
+
+    //        if (iSensPropagation == 1)
+    //        {
+
+    //            iIdChercher = iIdParent;
+
+    //        }
+    //        else if (iSensPropagation == 2)
+    //        {
+
+    //            iIdChercher
+
+    //        }
+
+    //    }
+
+    //    for (int j = 0; j < 50; j++)
+    //    {
+
+    //        for (int i = 0; i < 50; i++)
+    //        {
+
+    //            int iIdVerif = hMainCam.GetComponent<MainGame>().mTableJunction[i].GetComponent<Jonction>().iIdActuel;
+
+    //            if (iIdVerif == iIdChercher)
+    //            {
+    //                if (iEtat >= 0)
+    //                {
+    //                    hMainCam.GetComponent<MainGame>().mTableJunction[i].GetComponent<Jonction>().iEtat = hMainCam.GetComponent<MainGame>().mTableJunction[i].GetComponent<Jonction>().iEtat + 1;
+    //                }
+    //                else
+    //                {
+
+    //                    hMainCam.GetComponent<MainGame>().mTableJunction[i].GetComponent<Jonction>().iEtat = hMainCam.GetComponent<MainGame>().mTableJunction[i].GetComponent<Jonction>().iEtat - 1;
+
+    //                }
+
+
+
+    //                iIdChercher = hMainCam.GetComponent<MainGame>().mTableJunction[i].GetComponent<Jonction>().iIdParent;
+
+
+    //            }
+    //        }
+
+
+    //        if (iIdParent == 0)
+    //        {
+
+    //            j = 100;
+
+    //        }
+
+    //    }
+    }
+}
