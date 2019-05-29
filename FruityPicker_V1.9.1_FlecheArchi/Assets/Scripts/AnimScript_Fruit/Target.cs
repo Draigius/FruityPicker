24989cfdc67ea7532ca6b58f2eacc395c2a61d4d
Draigius <49942635+Draigius@users.noreply.github.com>
2019-05-29 15:06:40 +0200

    ajout pack end

---
+++ FruityPicker_V1.9.1_FlecheArchi/Assets/Scripts/AnimScript_Fruit/Target.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    Vector3 Pos;
+    float fTimer;
+    float fDelay = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Pos = new Vector3(Random.Range(-500f, 500f), Random.Range(-400f, 500f), Random.Range(-500f, -200f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fTimer += Time.deltaTime;
+
+        if (fTimer>fDelay)
+        {
+            transform.position = new Vector3(Random.Range(-500f, 500f), Random.Range(-400f, 500f), Random.Range(-500f, -200f));
+            Pos = new Vector3(Random.Range(-500f, 500f), Random.Range(-400f, 500f), Random.Range(-500f, -200f));
+            fDelay = 2f + Random.Range(-1.6f, 3f);
+            fTimer = 0;
+
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, Pos, 4f);
+
+        }
+        
+    }
+}
---
+++ FruityPicker_V1.9.1_FlecheArchi/Assets/Scripts/AnimScript_Fruit/feedbackArchi/ConectionBras.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConectionBras : MonoBehaviour
+{
+    [Range(0.1f, 5)]
+    public float fVitesseFleche = 0.5f;
+
+    public bool bActif = false;
+
+    public int iEtatFB = 0;
+
+    [Tooltip("1 = vers le haut ; \n 2 = vers le bas ;")]
+    public int iSensFB = 1;
+
+    int iMultiplicateur = 0;//dépent de iSens, permet de fait des calcules
+
+    [Header("ne pas toucher")]
+    [Header("________________________________________________________________________")]
+    
+    public GameObject hDummy0;
+    public GameObject hDummy1;
+    public GameObject hMeshFleche;
+
+
+    private int iSensFBSauvgarde;
+
+
+    public Material[] mTableMaterial = new Material[2];
+
+    Renderer rend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        funcRenduFleche(bActif);
+        Debug.Log("iSensFB :" + iSensFB);
+
+        if(iSensFBSauvgarde != iSensFB && iSensFB != 0)
+        {
+
+            hMeshFleche.GetComponent<Fleche>().hMesh.transform.Rotate(new Vector3(0, 180, 0), Space.Self);
+
+        }
+
+        iSensFBSauvgarde = iSensFB;
+
+
+
+
+        hMeshFleche.GetComponent<Fleche>().iSens = iMultiplicateur;
+
+
+        if (iSensFB == 1)
+        {
+
+            iMultiplicateur = -1;
+
+        }
+        else
+        {
+
+            iMultiplicateur = 1;
+
+        }
+
+
+        if(hMeshFleche.GetComponent<Fleche>().bStart == true)
+        {
+
+            hMeshFleche.transform.Translate(new Vector3(0, (fVitesseFleche * Time.deltaTime) * iMultiplicateur, 0), Space.Self);
+
+        }
+
+
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+
+            hMeshFleche.GetComponent<Fleche>().bStart = true;
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            funcChangeSens();
+
+        }
+
+    }
+
+
+
+    
+
+    public void funcChangeSens()
+    {
+
+        if(iSensFB == 1)
+        {
+
+            iSensFB = 2;
+
+        }
+        else
+        {
+
+            iSensFB = 1;
+
+        }
+        
+    }
+
+    public void funcStart()
+    {
+
+        hMeshFleche.GetComponent<Fleche>().funcRendu();
+
+    }
+
+
+
+    public void funcRenduFleche(bool bRenduActive)
+    {
+
+        rend = hMeshFleche.GetComponent<Fleche>().hMesh.GetComponent<Renderer>();
+        rend.enabled = bRenduActive;
+
+        rend.sharedMaterial = mTableMaterial[Mathf.Abs(iEtatFB)];
+        
+    }
+
+}
